87c56475956423ecb15d5346e3728831b6cc0e86
Sergey Chirkov <s.chirkov1@g.nsu.ru>
2025-10-22 01:41:48 +0700

    draw_hexagon and graw_hexagons function is done

--- draw_hexagon-function.py
+++ draw_hexagon-function.py
@@ -2,38 +2,79 @@ import turtle as t
 import math as m
 
 
-def draw_hexagon(q: int, first_color: str, second_color: str) -> None:
+def draw_hexagon(x: int, y: int, side_len: int, color: str) -> None:
     '''
-    Два цвета для окрашивания
-    количество (N) шестиугольников в одном ряду (от 4 до 20)
-    картинка 500x500 пикселей
-    Чередование цвета в каждой строке
+    function for drawing each hexagon
+
+    :param x:
+    :param y:
+    :param side_len:
+    :param color:
     :return:
     '''
-    d_2 = 500/(2*q+1)
-    d = d_2*2
-    l = (d_2/m.sqrt(3))*2
-    t.speed(100)
+    t.up()
+    t.goto(x, y)
+    t.down()
+    t.setheading(90)
+
+    t.fillcolor(color)
+    t.begin_fill()
+    for _ in range(6):
+        t.forward(side_len)
+        t.right(60)
+    t.end_fill()
 
 
+def draw_hexagons(q: int, first_color: str, second_color: str) -> None:
+    '''
+        Two colors to color
+    Number (N) of hexagons in one row (4 to 20)
+    500x500 pixel image
+    Alternating colors in each row
+
+        * * *
+      *       *
+    *           *
+      *       *
+        * * *
+
+    difference between top and botoom = 2l
+    d - diameter of the inscribed circle
+    d_2 - radius of the inscribed circle
+
+    :param q:
+    :param first_color:
+    :param second_color:
+    :return:
+    '''
+    d_2 = 500 / (2 * q + 1)
+    d = d_2 * 2
+    l = (d_2 / m.sqrt(3)) * 2
+    # turning off animation
+    t.tracer(0)
+
+    # Cycle for line-by-line drawing
     for numb_horiz in range(q):
         for numb_vert in range(q):
-            if numb_horiz%2 != 0:
-                t.teleport(-250 + d_2 + (d * numb_vert), (250 - (d_2 / 2) - (numb_horiz * d)) - l/2)
+            # Condition for checking a row.
+            # Depending on whether the row is even, the row should be shifted to the left or not.
+            if numb_horiz % 2 != 0:
+                x, y = -250 + (d * numb_vert), 250 - (l / 2) - (numb_horiz * 1.5 * l)
             else:
-                t.teleport(-250 + d + (d * numb_vert), (250 - (d_2 / 2) - (numb_horiz * d)) - l/4)
-            if numb_vert%2 != 0:
-                t.pen(pencolor='black', fillcolor=first_color, pensize=1)
+                x, y = -250 + d_2 + (d * numb_vert), 250 - (l / 2) - (numb_horiz * 1.5 * l)
+
+            # Condition for choosing the color of hexes so that there is a diagonal coloring.
+            # Group in pairs rows and watch on ewen.
+            if (numb_horiz // 2) % 2 != 0:
+                if numb_vert % 2 != 0:
+                    draw_hexagon(x, y, l, first_color)
+                else:
+                    draw_hexagon(x, y, l, second_color)
             else:
-                t.pen(pencolor='black', fillcolor=second_color, pensize=1)
-            t.down()
-            t.begin_fill()
-            t.left(30)
-            for _ in range(6):
-                t.forward(l)
-                t.right(60)
-            t.right(30)
-            t.up()
+                if numb_vert % 2 != 0:
+                    draw_hexagon(x, y, l, second_color)
+                else:
+                    draw_hexagon(x, y, l, first_color)
     t.mainloop()
 
 
@@ -41,4 +82,5 @@ q = 10
 first_color = 'red'
 second_color = 'blue'
 
-draw_hexagon(q, first_color, second_color)
+
+draw_hexagons(q, first_color, second_color)
